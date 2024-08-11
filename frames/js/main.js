74de35a1aceb67dabc8174eee750b4f2cf9b8482
themscool <design.themscool@gmail.com>
2024-08-11 05:36:22 +0530

    first commit

---
+++ frames/js/main.js
@@ -0,0 +1,77 @@
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+const frames = {
+  currentIndex: 0,
+  maxIndex: 1502,
+};
+let imagesLoaded = 0;
+
+const images = [];
+function preloadImages() {
+    for (var i = 1; i <= frames.maxIndex; i++) {
+        const imageUrl = `./frames/frame_${i.toString().padStart(4, "0")}.jpeg`;
+        const img = new Image();
+        img.src = imageUrl;
+        img.onload = () => {
+            imagesLoaded++;
+            if (imagesLoaded === frames.maxIndex) {
+                loadImage(frames.currentIndex);
+                startAnimation();
+            }
+        };
+        img.onerror = (e) => {
+            console.error(`Failed to load image: ${imageUrl}`, e);
+        };
+        images.push(img);
+    }
+}
+
+
+function loadImage(index) {
+    if (index >= 0 && index <= frames.maxIndex) {
+        const img = images[index];
+
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        const scaleX = canvas.width / img.width;
+        const scaleY = canvas.height / img.height;
+        const scale = Math.max(scaleX, scaleY);
+
+        const newWidth = img.width * scale;
+        const newHeight = img.height * scale;
+
+        const offsetX = (canvas.width - newWidth) / 2;
+        const offsetY = (canvas.height - newHeight) / 2;
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.imageSmoothingEnabled = true;
+        context.imageSmoothingQuality = "high";
+        context.drawImage(img, offsetX, offsetY, newWidth, newHeight);
+
+        frames.currentIndex = index;
+    }
+}
+
+
+function startAnimation() {
+    var tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".parent",
+            start: "top top",
+            scrub: 2,
+            end: "bottom bottom",
+            markers: true
+        }
+    });
+
+    tl.to(frames, {
+        currentIndex: frames.maxIndex,
+        onUpdate: function () {
+            loadImage(Math.floor(frames.currentIndex));
+        }
+    });
+}
+
+preloadImages ();
